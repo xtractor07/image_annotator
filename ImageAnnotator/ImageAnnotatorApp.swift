1d88b37f84a89872f4861c55127f75a8aac67672
xtractor07 <tryy.m3@gmail.com>
2024-02-22 17:16:18 +0530

    Initial Commit

---
+++ ImageAnnotator/ImageAnnotatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnnotatorApp.swift
+//  ImageAnnotator
+//
+//  Created by Kumar Aman on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnnotatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
